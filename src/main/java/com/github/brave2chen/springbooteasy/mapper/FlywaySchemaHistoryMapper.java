1c5d8ab4716a8125ac6c988750f21b2e1ab0cc6e
brave2chen <bravechen@foxmail.com>
2020-05-29 14:15:08 +0800

    SpringBoot 脚手架搭建系列之三：mybatis-plus集成和项目结构确定

---
+++ src/main/java/com/github/brave2chen/springbooteasy/mapper/FlywaySchemaHistoryMapper.java
@@ -0,0 +1,16 @@
+package com.github.brave2chen.springbooteasy.mapper;
+
+import com.github.brave2chen.springbooteasy.model.FlywaySchemaHistory;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author brave2chen
+ * @since 2020-05-29
+ */
+public interface FlywaySchemaHistoryMapper extends BaseMapper<FlywaySchemaHistory> {
+
+}
---
+++ src/main/java/com/github/brave2chen/springbooteasy/service/FlywaySchemaHistoryService.java
@@ -0,0 +1,19 @@
+package com.github.brave2chen.springbooteasy.service;
+
+import com.github.brave2chen.springbooteasy.model.FlywaySchemaHistory;
+import com.github.brave2chen.springbooteasy.mapper.FlywaySchemaHistoryMapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  Service
+ * </p>
+ *
+ * @author brave2chen
+ * @since 2020-05-29
+ */
+@Service
+public class FlywaySchemaHistoryService extends ServiceImpl<FlywaySchemaHistoryMapper, FlywaySchemaHistory> {
+
+}
