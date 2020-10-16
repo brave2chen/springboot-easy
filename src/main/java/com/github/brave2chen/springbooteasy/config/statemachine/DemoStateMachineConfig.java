e2e537120570b4c3495e1af0dedbd65406c80df9
brave2chen <bravechen@foxmail.com>
2020-10-16 08:05:21 +0800

    集成状态机，并编写demo样

--- src/main/java/com/github/brave2chen/springbooteasy/config/statemachine/DemoStateMachineConfig.java
+++ src/main/java/com/github/brave2chen/springbooteasy/config/statemachine/DemoStateMachineConfig.java
@@ -1,11 +1,15 @@
 package com.github.brave2chen.springbooteasy.config.statemachine;
 
+import cn.hutool.core.util.RandomUtil;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.StateMachine;
-import org.springframework.statemachine.annotation.OnStateChanged;
+import org.springframework.statemachine.action.Action;
+import org.springframework.statemachine.annotation.OnStateMachineError;
 import org.springframework.statemachine.annotation.OnTransition;
 import org.springframework.statemachine.annotation.WithStateMachine;
 import org.springframework.statemachine.config.EnableStateMachine;
@@ -13,14 +17,13 @@ import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.guard.Guard;
 import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
-import org.springframework.statemachine.transition.Transition;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.EnumSet;
-import java.util.Optional;
 
 /**
  * 状态及样例
@@ -33,11 +36,11 @@ import java.util.Optional;
 public class DemoStateMachineConfig extends EnumStateMachineConfigurerAdapter<DemoStateMachineConfig.States, DemoStateMachineConfig.Events> {
 
     public enum States {
-        INIT, COMMITTED, AUDITED, ARCHIVED
+        TO_CREATE, TO_MODIFY, SAVED, COMMITTED, AUDITED, ARCHIVED, TRANSITION
     }
 
     public enum Events {
-        COMMIT, AUDIT, ARCHIVE
+        SAVE, COMMIT, SAVE_AND_COMMIT, AUDIT, AUDIT_FAIL, ARCHIVE
     }
 
 
@@ -73,8 +76,15 @@ public class DemoStateMachineConfig extends EnumStateMachineConfigurerAdapter<De
     public void configure(StateMachineStateConfigurer<States, Events> states) throws Exception {
         states
                 .withStates()
-                .initial(States.INIT)
+                .initial(States.TO_CREATE, action())
                 .end(States.ARCHIVED)
+                .stateDo(States.AUDITED, context -> System.err.println("AUDITED State action"))
+                .stateEntry(
+                        States.AUDITED,
+                        context -> { throw new RuntimeException("AUDITED Entry action");},
+                        context -> System.err.println("AUDITED Entry action error:" + context.getException().getMessage())
+                )
+                .stateExit(States.AUDITED, context -> System.err.println("AUDITED Exit action"))
                 .states(EnumSet.allOf(States.class));
     }
 
@@ -82,17 +92,49 @@ public class DemoStateMachineConfig extends EnumStateMachineConfigurerAdapter<De
     public void configure(StateMachineTransitionConfigurer<States, Events> transitions) throws Exception {
         transitions
                 .withExternal()
-                .source(States.INIT).target(States.COMMITTED)
-                .event(Events.COMMIT)
+                .source(States.TO_CREATE).target(States.SAVED).event(Events.SAVE)
                 .and()
                 .withExternal()
-                .source(States.COMMITTED).target(States.AUDITED)
-                .event(Events.AUDIT)
+                .source(States.TO_CREATE).target(States.COMMITTED).event(Events.SAVE_AND_COMMIT)
                 .and()
                 .withExternal()
-                .source(States.AUDITED).target(States.ARCHIVED)
-                .event(Events.ARCHIVE)
+                .source(States.SAVED).target(States.COMMITTED).event(Events.COMMIT)
+                .and()
+                .withExternal()
+                .source(States.COMMITTED).target(States.AUDITED).event(Events.AUDIT)
+                .and()
+                .withExternal()
+                .source(States.COMMITTED).target(States.TO_MODIFY).event(Events.AUDIT_FAIL)
+                .and()
+                .withExternal()
+                .source(States.TO_MODIFY).target(States.SAVED).event(Events.SAVE)
+                .and()
+                .withExternal()
+                .source(States.TO_MODIFY).target(States.COMMITTED).event(Events.SAVE_AND_COMMIT)
+                .and()
+                .withExternal()
+                .source(States.AUDITED).target(States.ARCHIVED).event(Events.ARCHIVE)
         ;
+    }
+
+    public Guard<States, Events> guard(boolean reverse) {
+        return new Guard<States, Events>() {
+            @Override
+            public boolean evaluate(StateContext<States, Events> context) {
+                return  (boolean) context.getMessage().getHeaders().get("result") && reverse;
+            }
+        };
+    }
+
+    @Bean
+    public Action<States, Events> action() {
+        return new Action<States, Events>() {
+
+            @Override
+            public void execute(StateContext<States, Events> context) {
+                System.err.println("action: " + context.getStateMachine().getState());
+            }
+        };
     }
 
     @WithStateMachine(name = "demoStateMachine")
@@ -101,9 +143,14 @@ public class DemoStateMachineConfig extends EnumStateMachineConfigurerAdapter<De
             System.err.println("init DemoStateMachineBusiness -------------------");
         }
 
-        @OnStateChanged(target = "ARCHIVED")
-        public void toState2() {
-            System.err.println("@OnStateChanged: ---> ARCHIVED");
+        @OnTransition(target = "ARCHIVED")
+        public void toState2(Message<Events> message) {
+            System.err.println("@OnTransition: ---> ARCHIVED, message " + message);
+        }
+
+        @OnStateMachineError
+        public void OnStateMachineError(Exception e) {
+            System.err.println(e.getMessage());
         }
     }
 
@@ -122,6 +169,15 @@ public class DemoStateMachineConfig extends EnumStateMachineConfigurerAdapter<De
 
         @Override
         public void run(String... args) throws Exception {
+            demoStateMachine.sendEvent(Events.ARCHIVE);
+
+            demoStateMachine.sendEvent(Events.SAVE);
+            Message<Events> commit = MessageBuilder.withPayload(Events.COMMIT).setHeader("result", RandomUtil.randomBoolean()).build();
+            demoStateMachine.sendEvent(commit);
+            demoStateMachine.sendEvent(Events.AUDIT_FAIL);
+            demoStateMachine.sendEvent(Events.SAVE_AND_COMMIT);
+            demoStateMachine.sendEvent(Events.AUDIT_FAIL);
+            demoStateMachine.sendEvent(Events.SAVE);
             demoStateMachine.sendEvent(Events.COMMIT);
             demoStateMachine.sendEvent(Events.AUDIT);
             demoStateMachine.sendEvent(Events.ARCHIVE);
