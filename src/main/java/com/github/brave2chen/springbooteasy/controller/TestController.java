798fd038cf7149cfc8a6efe40c20eb1d3b2dd9de
brave2chen <bravechen@foxmail.com>
2021-02-02 20:55:10 +0800

    feat: 添加Micrometer，并实现自定义指标监控demo

--- src/main/java/com/github/brave2chen/springbooteasy/controller/TestController.java
+++ src/main/java/com/github/brave2chen/springbooteasy/controller/TestController.java
@@ -1,11 +1,16 @@
 package com.github.brave2chen.springbooteasy.controller;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 /**
  * 测试
@@ -19,8 +24,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
 public class TestController {
 
+    @Autowired
+    private MeterRegistry meterRegistry;
+
+    private Counter counter;
+
+    @PostConstruct
+    private void init() {
+        counter = meterRegistry.counter("app_requests_method_count", "method", "hello");
+    }
+
     @RequestMapping(value = "", method = {RequestMethod.GET, RequestMethod.POST})
     public String hello() {
+        try {
+            counter.increment();
+        } catch (Exception e) {
+        }
         return "你有权限访问";
     }
 }
